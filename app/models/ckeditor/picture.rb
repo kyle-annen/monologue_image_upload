a72f4ca3ad62bfadcfd69e0dfa18e99789603993
Kyle Annen <kyleannen@gmail.com>
2015-07-31 21:06:20 -0700

    Update picture.rb to use AWS S3

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,8 +1,14 @@
 class Ckeditor::Picture < Ckeditor::Asset
-  has_attached_file :data,
-                    :url  => "/#{config.upload_path}/pictures/:id/:style_:basename.:extension",
+  has_attached_file :data, 
+                    :styles => { :content => '800>', :thumb => '118x100#'},
+                    :storage  => :s3,
+                    :s3_credentials => {
+                      :bucket => ENV['S3_BUCKET'],
+                      :access_key_id => ENV['S3_KEY'],
+                      :secret_access_key => ENV['S3_SECRET']
+                    },
                     :path => ":rails_root/public/#{config.upload_path}/pictures/:id/:style_:basename.:extension",
-                    :styles => { :content => '800>', :thumb => '118x100#' }
+                    :url => ":s3_domain_url"
 
   validates_attachment :data,
                        :presence => true,
